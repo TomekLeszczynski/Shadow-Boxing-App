09bbbc4fc779bba434cd4bf461648dfd78502f98
Tomek Leszczynski <hiszpania2018@gmail.com>
2023-12-30 16:22:03 +0100

    Vuelify dependency added

--- src/components/login_register/RegisterNewUser.vue
+++ src/components/login_register/RegisterNewUser.vue
@@ -2,23 +2,6 @@
   <div class="bg-black flex-grow">
     <div class="max-w-screen-lg mx-auto h-full flex-grow flex justify-center items-center">
       <div class="bg-brickRed p-5 md:p-8">
-        <div class="font-bold text-md md:text-lg flex flex-row group tracking-tight">
-          <span
-            ><svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6 pr-1 group-hover:-translate-x-3 duration-300 ease-in-out"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-              /></svg></span
-          >Go to back
-        </div>
         <h2 class="text-black md:text-9xl text-6xl font-bold py-6">CREATE ACCOUNT</h2>
         <div class="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
           <div class="lg:col-span-5">
@@ -27,57 +10,68 @@
               class="gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5"
             >
               <div class="md:col-span-5 mb-3">
-                <label for="displayName" class="font-bold">Username</label>
+                <label for="displayName" class="font-semibold" pattern="^[^0-9]+$">Username</label>
                 <input
+                  id="displayName"
                   v-model="displayName"
                   type="text"
-                  name="displayName"
                   class="h-10 my-1 px-4 w-full bg-white placeholder:font-normal border-none"
+                  autocomplete="off"
                   required
                   placeholder="Rocky Balboa"
                 />
-                <p class="text-xs text-black">Render if not ok</p>
+                <p class="text-xs text-white">Render if not ok</p>
               </div>
 
               <div class="md:col-span-5 mb-3">
-                <label for="email">Email</label>
+                <label for="email" class="font-semibold">Email</label>
                 <input
+                  id="email"
                   v-model="email"
                   type="email"
-                  name="email"
-                  class="h-10 mt-1 px-4 w-full bg-gray-50"
+                  class="h-10 my-1 px-4 w-full bg-white placeholder:font-normal border-none"
+                  pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                  autocomplete="off"
                   required
                   placeholder="example@mail.com"
                 />
-                <p class="text-xs text-red-500">Render if not ok</p>
+                <p class="text-xs text-white">Render if not ok</p>
               </div>
 
               <div class="md:col-span-5 mb-3">
-                <label for="address">Password</label>
+                <label for="password" class="font-semibold">Password</label>
                 <input
+                  id="password"
                   v-model="password"
                   type="password"
-                  name="password"
-                  class="h-10 mt-1px-4 w-full bg-gray-50"
+                  class="h-10 my-1 px-4 w-full bg-white placeholder:font-normal border-none"
+                  title="Please enter a password that is at least 8 characters long and includes a mix of letters, numbers, and special characters."
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+                  autocomplete="off"
                   required
                 />
-                <p class="text-xs text-red-500">Render if not ok</p>
+                <p class="text-xs text-white">Render if not ok</p>
               </div>
               <div class="md:col-span-5 mb-3">
-                <label for="address">Confirm Password</label>
+                <label for="confirmPassword" class="font-semibold">Repeat Password</label>
                 <input
+                  id="confirmPassword"
+                  v-model="confirmPassword"
                   type="password"
-                  name="password"
-                  class="h-10 mt-1 px-4 w-full bg-gray-50"
+                  class="hh-10 my-1 px-4 w-full bg-white placeholder:font-normal border-none"
+                  title="Please confirm your password by entering it again."
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+                  autocomplete="off"
                   required
                 />
-                <p class="text-xs text-red-500">Render if not ok</p>
+                <p class="ttext-xs text-white">Render if not ok</p>
               </div>
 
               <div class="md:col-span-5 text-right">
-                <div class="inline-flex items-end">
+                <div class="inline-flex items-end py-6">
                   <button
-                    class="bg-black text-white text-lg font-bold py-4 px-6 flex flex-row items-center group tracking-wide"
+                    :disabled="!readyToGo"
+                    class="bg-black text-white text-lg font-semibold py-4 px-12 flex flex-row items-center group tracking-wide"
                   >
                     Submit
                     <span class="group-hover:translate-x-3 transition duration-300 ease-in-out"
@@ -108,7 +102,7 @@
 </template>
 <script setup lang="ts">
 // vue import
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 // router import
 import { useRouter } from 'vue-router'
@@ -125,11 +119,21 @@ const authStore = useAuthStore()
 // user data
 const email = ref<string>('')
 const password = ref<string>('')
+const confirmPassword = ref<string>('')
 const displayName = ref<string>('')
+
+
+// submit form controller
+let readyToGo = ref<boolean>(false)
+
+// validate user data
+
+
+
 
 // registering new user with required data: email, password, username
 const registerUser = (): void => {
-  // verify input values
+  // validate user data
   // ...
 
   // create user on firebase database
