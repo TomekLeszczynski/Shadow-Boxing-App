0217403f9584f95913ef153334ec35662fa1026d
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-14 21:48:15 +0100

    try as guest adjusted to other CTA button in home view

--- src/components/login_register/TryAsGuest.vue
+++ src/components/login_register/TryAsGuest.vue
@@ -1,5 +1,15 @@
 <template>
-  <button @click.prevent="getAccess" >Try as guest</button>
+  <button
+    @click.prevent="getAccess"
+    aria-label="log in"
+    role="button"
+    class="bg-black text-white text-md md:text-lg font-semibold mt-5 py-4 md:py-5 md:px-6 flex items-center justify-center group tracking-wide w-full animate-[flyInLeft_900ms_ease-out_900ms_both]"
+  >
+    <button-label
+      labelText="Try As Guest"
+      class="animate-[flyIn_900ms_ease-out_900ms_both]"
+    />
+  </button>
 </template>
 <script setup lang="ts">
 // router import
@@ -7,6 +17,9 @@ import { useRouter } from 'vue-router'
 
 // pinia import
 import { useAuthStore } from '@/stores/AuthentificationStore'
+
+// component import
+import ButtonLabel from '@/components/shared/ButtonLabel.vue'
 
 const router = useRouter()
 const authStore = useAuthStore()
