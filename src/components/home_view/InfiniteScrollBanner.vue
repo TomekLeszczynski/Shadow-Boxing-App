a072489d3bfa07141db08eb1ec72bf618be8e25f
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-14 21:44:57 +0100

    Infinite scroll banner moved to separate component

---
+++ src/components/home_view/InfiniteScrollBanner.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex">
+    <div
+      class="text-6xl text-white whitespace-nowrap font-semibold py-2 md:py-4 animate-infinite-scroll pl-5"
+    >
+      {{ bannerText }}
+    </div>
+    <div
+      class="text-6xl text-white whitespace-nowrap font-semibold py-2 md:py-4 animate-infinite-scroll pl-5"
+    >
+      {{ bannerText }}
+    </div>
+    <div
+      class="text-6xl text-white whitespace-nowrap font-semibold py-2 md:py-4 animate-infinite-scroll pl-5"
+    >
+      {{ bannerText }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(['bannerText'])
+</script>
