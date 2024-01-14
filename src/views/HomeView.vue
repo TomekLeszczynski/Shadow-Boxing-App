d02329814e1318a73fc2b58801bd1047ab41a49b
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-14 21:50:44 +0100

    content added. hero image replaced. CTA buttons description added. infinite scroll banner moved to seprate component and imported.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,95 +1,90 @@
 <template>
   <div class="flex h-full overflow-hidden">
-    <div class="grid grid-cols-1 gap-4">
-      <h1 class="text-white font-black animate-headerFlyIn text-9xl mb-4 text-left">
+    <div class="grid grid-cols-1 gap-2 md:gap-3 p-2">
+      <!-- MAIN HEADER -->
+      <h1
+        class="text-white font-black animate-headerFlyIn text-5xl sm:text-6xl md:text-7xl lg:text-9xl mb-4 text-left"
+      >
         SHADOW BOXING WORKOUT
       </h1>
-      <div class="grid grid-cols-2 gap-4">
-        <div class="bg-boxingGreen p-9 animate-flyInLeft h-auto">
+      <div class="grid grid-cols-full lg:grid-cols-2 gap-2 md:gap-3">
+        <div class="bg-boxingGreen p-4 md:p-9 animate-flyInLeft h-auto">
           <div class="grid grid-cols-full content-end">
-            <h2 class="text-8xl font-bold mb-9 leading-none">
-              <div class="animate-[flyIn_1.3s_ease_both]">HEADER</div>
-              <div class="animate-[flyIn_1.5s_ease_both]">TEXT</div>
-              <div class="animate-[flyIn_1.7s_ease_both]">TEST</div>
+            <!-- SMALL HEADER -->
+            <h2 class="text-6xl sm:text-7xl md:text-8xl font-bold mb-5 md:mb-9">
+              <div class="animate-[flyIn_1.3s_ease_both]">FOR</div>
+              <div class="animate-[flyIn_1.5s_ease_both]">THE LOVE</div>
+              <div class="animate-[flyIn_1.7s_ease_both]">OF BOXING</div>
             </h2>
-            <p class="text-xl animate-[flyIn_900ms_ease_both]">
-              Follow displayed commands and throw or slip away from the punches. Develop your boxing
-              combos, footwork and head movement!
-            </p>
-            <div class="grid gap-12 grid-cols-2 mb-10">
-              <div>
-                <button
-                  aria-label="log in"
-                  role="button"
-                  class="bg-black text-white text-lg font-semibold mt-5 py-5 px-9 flex flex-row items-center justify-center group tracking-wide w-full animate-[flyInLeft_900ms_ease_both]"
-                >
-                  <button-label
+            <!-- DESCRIPTION 01 -->
+            <div class="text-md sm:text-lg md:text-2xl animate-[flyIn_900ms_ease_both] font-thin">
+              <p class="text-wrap mb-3">
+                Shadow Boxing Workout is an innovative app that takes boxing training to a new
+                level. With our app, you can practice 'shadow boxing,' refining your combinations,
+                footwork, and head movement.
+              </p>
+              <p class="mb-3">
+                Whether you're an experienced boxer or just starting your journey in the sport, our
+                app adapts to your skill level. Additionally, track your progress by saving your
+                workouts and weight measurements, allowing you to monitor your achievements.
+              </p>
+              <p class="font-semibold">Explore the world of boxing in a completely new way.</p>
+            </div>
+
+            <!-- ACCESS-BUTTONS CONTAINER -->
+            <div class="grid gap-4 lg:gap-8 grid-cols-1 sm:grid-cols-2 my-5 md:my-10">
+              <!-- GET STARTED CONTAINER -->
+              <div class="w-full flex flex-col justify-between">
+                <div class="text-white">
+                  <p>
+                    * Unlock the full experience by creating an account. Save your training sessions
+                    and weight measurements for personalized tracking and progress analysis.
+                  </p>
+                </div>
+                <!-- GET STARTED BUTTON -->
+                <router-link
+                  to="/signup"
+                  class="bg-black text-white text-md md:text-lg font-semibold mt-5 py-4 md:py-5 md:px-6 flex items-center justify-center group tracking-wide w-full animate-[flyInLeft_900ms_ease-out_900ms_both]"
+                  ><button-label
                     labelText="Get Started"
-                    class="animate-[flyIn_900ms_ease_both] w-full font-thin"
+                    class="animate-[flyIn_900ms_ease-out_900ms_both] w-full font-thin"
                   />
-                </button>
+                </router-link>
               </div>
-              <div>
-                <button
-                  aria-label="log in"
-                  role="button"
-                  class="bg-black text-white text-lg font-semibold mt-5 py-5 px-9 flex flex-row items-center justify-center group tracking-wide w-full animate-[flyInLeft_900ms_ease_both]"
-                >
-                  <button-label
-                    labelText="Try As Guest"
-                    class="animate-[flyIn_900ms_ease_both] w-full font-thin"
-                  />
-                </button>
+              <!-- TRY AS GUEST CONTAINER -->
+              <div class="w-full flex flex-col justify-between">
+                <div class="text-white">
+                  <p>
+                    * Instantly access all features without the need for an account. Explore the
+                    app's functionality, but keep in mind that data won't be saved without
+                    registration.
+                  </p>
+                </div>
+                <!-- TRY-AS-GUEST BUTTON -->
+                <try-as-guest />
               </div>
             </div>
           </div>
         </div>
-        <div class="animate-flyInLeft bg-small-hero bg-cover bg-center h-auto"></div>
-        <!-- <div class="flex-col flex ml-9 mt-10">
-          <div class="flex-row flex justify-between">
-            <router-link
-              to="/signup"
-              class="text-white bg-boxingRed flex justify-center items-center min-w-48 py-3"
-              >Get Started</router-link
-            >
-            <info-modal :modalText="getStartedModalText" />
-          </div>
-          <div class="flex-row flex mt-5">
-            <try-as-guest
-              class="text-white bg-boxingRed flex justify-center items-center min-w-48 py-3"
-            />
-            <info-modal :modalText="tryOutModalText" />
-          </div>
-        </div> -->
+        <!-- HERO IMG -->
+        <div
+          class="animate-flyInLeft bg-small-hero bg-cover bg-center h-0 md:h-auto"
+          alt="Young man in boxing gloves is looking up"
+        ></div>
       </div>
+      <!-- INFINITE SCROLL BANNER -->
       <div class="bg-boxingPurple overflow-hidden grid grid-cols-1 animate-headerFlyIn">
-        <div class="flex">
-          <div
-            class="text-5xl text-white whitespace-nowrap font-semibold py-4 animate-infinite-scroll pl-5"
-          >
-            ONLINE STORE AND BLOG SECTIONS COMING SOON!
-          </div>
-          <div
-            class="text-5xl text-white whitespace-nowrap font-semibold py-4 animate-infinite-scroll pl-5"
-          >
-            ONLINE STORE AND BLOG SECTIONS COMING SOON!
-          </div>
-          <div
-            class="text-5xl text-white whitespace-nowrap font-semibold py-4 animate-infinite-scroll pl-5"
-          >
-            ONLINE STORE AND BLOG SECTIONS COMING SOON!
-          </div>
-        </div>
+        <infinite-scroll-banner bannerText="ONLINE STORE AND BLOG SECTIONS COMING SOON!" />
       </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+// vue-router import
 import { RouterLink } from 'vue-router'
-import TryAsGuest from '@/components/login_register/TryAsGuest.vue'
-import InfoModal from '@/components/shared/InfoModal.vue'
-import ButtonLabel from '@/components/login_register/supportComponents/ButtonLabel.vue'
 
-const getStartedModalText = 'This is "Get Started" modal info text!'
-const tryOutModalText = 'This is "Try Out" modal info text!'
+// component import
+import TryAsGuest from '@/components/login_register/TryAsGuest.vue'
+import ButtonLabel from '@/components/shared/ButtonLabel.vue'
+import InfiniteScrollBanner from '@/components/home_view/InfiniteScrollBanner.vue'
 </script>
