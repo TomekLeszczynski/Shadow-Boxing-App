8c9afd217e35aea8c664f9087f429b9434a564c7
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-16 22:43:15 +0100

    opacity adjusted

--- src/components/site_structure/footer/FlaticonCredits.vue
+++ src/components/site_structure/footer/FlaticonCredits.vue
@@ -1,5 +1,6 @@
 <template>
-  <a class="text-xs"
+  <a
+    class="text-xs opacity-20 text-gray-100"
     href="https://www.flaticon.com/free-icons/fight"
     title="fight icons"
     >Fight icons created by Freepik - Flaticon</a
