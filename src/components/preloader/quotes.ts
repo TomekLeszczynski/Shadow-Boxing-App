8d92b3bfc87298f0d9a00ae3464aa15511365275
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-21 21:22:31 +0100

    preloader quotes updated

---
+++ src/components/preloader/quotes.ts
@@ -0,0 +1,56 @@
+const preloaderQuotes = [
+  { quote: '“Float like a butterfly, sting like a bee.”', author: 'Muhammad Ali' },
+  {
+    quote:
+      '"Champions aren’t made in the gyms. Champions are made from something they have deep inside them—a desire, a dream, a vision."',
+    author: 'Joe Frazier'
+  },
+  {
+    quote:
+      '"It’s not about how hard you can hit, it’s about how hard you can get hit and keep moving forward."',
+    author: 'Rocky Balboa'
+  },
+  {
+    quote:
+      '"I hated every minute of training, but I said - Don’t quit. Suffer now and live the rest of your life as a champion."',
+    author: 'Muhammad Ali'
+  },
+  { quote: '"A champion is someone who gets up when they can’t."', author: 'Jack Dempsey' },
+  {
+    quote:
+      '"Every time I step into the ring, I’m looking for a knockout. I’m there to make a statement."',
+    author: 'Mike Tyson'
+  },
+  {
+    quote:
+      '"Fighting is not physical, fighting is spiritual. It’s the determination and the will in the guy"',
+    author: 'Mike Tyson'
+  },
+  {
+    quote: '"Knowing is not enough, we must apply. Willing is not enough, we must do."',
+    author: 'Bruce Lee'
+  },
+  {
+    quote: '"Use only that which works, and take it from any place you can find it."',
+    author: 'Bruce Lee'
+  },
+  {
+    quote: '"Take things as they are. Punch when you have to punch. Kick when you have to kick."',
+    author: 'Bruce Lee'
+  },
+  {
+    quote: '"My opponent is my teacher. My ego is my enemy."',
+    author: 'Unknown'
+  },
+  {
+    quote:
+      '"The fighter who give their all will be around next year. Don’t save anything. Give it all you got."',
+    author: 'George Foreman'
+  },
+  {
+    quote: '"Train with the energy of a warrior and soon you will have the heart of a warrior."',
+    author: 'Shifu Yan Lei '
+  }
+]
+
+export default preloaderQuotes
