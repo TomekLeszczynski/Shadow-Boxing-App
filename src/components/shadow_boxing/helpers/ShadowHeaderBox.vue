9f987406ba5218dde618e71939053258724dec68
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-27 23:02:59 +0100

    shadow boxing view set

--- src/components/shadow_boxing/helpers/ShadowHeaderBox.vue
+++ src/components/shadow_boxing/helpers/ShadowHeaderBox.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="text-almost-white font-semibold font-public-sans animate-text-show-up-03">
+  <div class="text-almost-white font-semibold font-public-sans">
     <!-- <h1
       class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[9rem] tracking-tighter py-3 mb-12"
     >
       Select Level
     </h1> -->
-    <h1 class="text-7xl my-9 text-justify">
-      Set the workout, follow displayed commands and throw or slip away from the punches. Don’t
-      forget about your footwork and head movement!
+    <h1 class="text-2xl sm:text-4xl lg:text-6xl my-9 text-justify">
+      Set up your shadow-boxing session, follow the commands to execute punches or slips
+      accordingly. Remember to maintain active footwork and head movement.
     </h1>
   </div>
 </template>
