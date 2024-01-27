649460d13b8a9442367e0278a1d81a8c6b8f5b3b
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-27 23:03:14 +0100

    footer updated

--- src/components/site_structure/footer/AuthorCredits.vue
+++ src/components/site_structure/footer/AuthorCredits.vue
@@ -1,9 +1,13 @@
 <template>
-  <p class="text-md font-public-sans text-almost-white"> &copy;{{ currentDate }} / Tomasz Leszczyński
-  </p>
-  <a class="size-8 " href="https://github.com/TomekLeszczynski" target="_blank"
+  <div class="flex flex-row mx-auto justify-center items-center gap-9">
+    <p class="text-md font-public-sans text-eagle">&copy;{{ currentDate }} / Tomasz Leszczyński</p>
+    <a
+      class="size-8 hover:scale-90 hover:duration-200"
+      href="https://github.com/TomekLeszczynski"
+      target="_blank"
       ><img src="@/assets/icons/github-mark-white.png" alt="github icon"
     /></a>
+  </div>
 </template>
 
 <script setup lang="ts">
