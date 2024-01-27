cdfe714364770c9f711769aaf6f46566f5d50554
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-27 22:58:46 +0100

    not found componen set

---
+++ src/components/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col h-full w-full justify-center items-center p-4">
+    <h1 class="text-red-500 font-semibold font-public-sans leading-none text-center">
+      <p class="text-5xl sm:text-7xl md:text-8xl xl:text-9xl">We're sorry, Champ!</p>
+      <p class="text-8xl sm:text-[10rem] md:text-[14rem] xl:text-[25rem]">404</p>
+      <p class="text-5xl sm:text-7xl md:text-8xl xl:text-9xl">Page not found!</p>
+    </h1>
+    <img
+      src="@/assets/photos/404-hero.webp"
+      alt="halftone-image-young-boxer-looking-up"
+      class="absolute h-full object-cover opacity-35 -z-10 right-2"
+    />
+  </div>
+</template>
