84bc58be95ef1f900a048879748f2261dbfd6529
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-27 23:01:06 +0100

    home hero image adjusted

--- src/components/home_view/HeroBox.vue
+++ src/components/home_view/HeroBox.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex flex-col relative h-60 lg:h-auto mb-6 md:mb-0">
-    <div class="animate-appear-slow-and-delayed h-full">
+  <div class="flex flex-col lg:h-full lg:mb-6 md:mb-0 overflow-hidden relative">
+    <div class="animate-appear-slow-and-delayed lg:h-full">
       <img
         src="@/assets/photos/hero-halftone_opt.webp"
         alt="halftone-image-woman-during-boxing-training"
-        class="absolute inset-0 object-cover h-full w-full opacity-35 -z-10"
+        class="absolute inset-0 h-full w-full object-cover hidden lg:block opacity-35 -z-10"
       />
     </div>
     <!-- SMALL HEADER -->
     <h2
-      class="text-4xl xl:text-5xl font-public-sans text-almost-white pb-2 z-10 animate-text-show-up-02 border-b"
+      class="text-4xl xl:text-5xl font-public-sans text-almost-white pb-2 z-10 animate-text-show-up-02 border-b hidden lg:block"
       aria-label="For the Love of Boxing"
     >
       <p>For</p>
@@ -18,8 +18,3 @@
     </h2>
   </div>
 </template>
-<script setup lang="ts">
-// component import
-// import GetAccess from '@/components/home_view/GetAccess.vue'
-// import DescriptionBox from '@/components/home_view/DecriptionBox.vue'
-</script>
