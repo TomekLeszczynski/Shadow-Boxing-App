c32e5bee3ff5f52a963ef186d117d8ac0021ff9f
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-27 23:05:32 +0100

    set workout component set

---
+++ src/views/SetWorkout.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="grid grid-cols-1 items-center mx-auto">
+    <level-configurator />
+  </div>
+</template>
+<script setup lang="ts">
+import LevelConfigurator from '@/components/shadow_boxing/helpers/LevelConfigurator.vue'
+</script>
