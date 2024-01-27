1209d92eabb48fec0040e0c5f6857bced81b82c4
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-01-27 23:00:02 +0100

    description-box text updated

--- src/components/home_view/GetAccess.vue
+++ src/components/home_view/GetAccess.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="grid gap-4 lg:gap-8 grid-cols-1 sm:grid-cols-2 mt-16">
+  <div class="grid gap-4 lg:gap-8 grid-cols-1 sm:grid-cols-2 lg:mt-16 md:mt-6">
     <!-- GET STARTED CONTAINER -->
     <div class="flex flex-col justify-between">
-      <p class="text-almost-white animate-text-show-up-02 text-justify mt-5 md:mt-0">
-        * Unlock the full experience by creating an account. Save your training sessions and weight
+      <p class="text-almost-white animate-text-show-up-02 text-justify mt-5 md:mt-0 text-xs md:text-base">
+        Unlock the full experience by creating an account. Save your training sessions and weight
         measurements for personalized tracking and progress analysis.
       </p>
       <!-- GET STARTED BUTTON -->
       <router-link
         to="/signup"
-        class="bg-almost-white text-almost-black text-md md:text-lg font-semibold mt-5 py-4 md:py-5 md:px-6 flex items-center justify-center group tracking-wide animate-button-show-from-left"
+        class="bg-red-500 text-almost-white text-md md:text-lg font-semibold mt-5 py-4 md:py-5 md:px-6 flex items-center justify-center group tracking-wide animate-button-show-from-left"
         role="button"
         aria-label="Get Started - Unlock the full experience by creating an account"
         ><button-label labelText="Get Started" />
@@ -17,8 +17,8 @@
     </div>
     <!-- TRY AS GUEST CONTAINER -->
     <div class="flex flex-col justify-between">
-      <p class="text-almost-white animate-text-show-up-02 text-justify mt-5 md:mt-0">
-        * Instantly access all features without the need for an account. Explore the app's
+      <p class="text-almost-white animate-text-show-up-02 text-justify mt-5 md:mt-0 text-xs md:text-base">
+        Instantly access all features without the need for an account. Explore the app's
         functionality, but keep in mind that data won't be saved without registration.
       </p>
       <!-- TRY-AS-GUEST BUTTON -->
