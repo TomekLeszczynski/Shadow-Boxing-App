061f1f5f0a7b7c534a188e10910ecb411b524d84
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-02-17 13:45:24 +0100

    new component name

---
+++ src/components/shadow_boxing/LevelSelector.vue
@@ -0,0 +1,17 @@
+<template>
+  <button title="click to select" class="group cursor-pointer animate-button-show-from-left">
+    <div class="p-4 h-full">
+      <p class="font-bold text-2xl md:text-4xl text-left relative md:px-5">
+        {{ level }}
+      </p>
+      <div class="text-sm">
+        <div class="text-justify md:px-6 md:py-4">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </button>
+</template>
+<script setup lang="ts">
+defineProps<{ level: string }>()
+</script>
