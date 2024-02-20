88044ec3ead11c354dbfcc4db7e1ccedccfa1ae3
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-02-20 21:47:28 +0100

    session config configurator labels set

---
+++ src/components/shadow_boxing/helpers/AdvancedConfigLabel.vue
@@ -0,0 +1,9 @@
+<template>
+  <label
+    class="text-lg block cursor-pointer select-none py-2 text-center bg-almost-white bg-opacity-20 peer-checked:bg-jaffa peer-checked:font-bold peer-checked:text-almost-white hover:bg-almost-grey hover:bg-opacity-20"
+    >{{ buttonLabel }}</label
+  >
+</template>
+<script setup lang="ts">
+defineProps<{ buttonLabel: string }>()
+</script>
