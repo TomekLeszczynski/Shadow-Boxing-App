1aca22f8eb3459e0da90d5c277c480a68270a1ca
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-02-20 21:48:11 +0100

    advanced configurator rounds-amount-decrease button set

---
+++ src/components/shadow_boxing/helpers/DecreaseButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="md:w-1/4 flex-1 text-2xl text-almost-black py-1 hover:bg-almost-grey hover:bg-opacity-20 active:text-almost-white active:bg-jaffa duration-100 flex justify-center items-center"
+  >
+    <span> &#8722; </span>
+  </button>
+</template>
