4f091bf1767249ff6d3fb4360d589d73cb9e31f0
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-02-20 21:50:31 +0100

    shadow boxing view total decomposition: code splitted

--- src/views/ShadowBoxingView.vue
+++ src/views/ShadowBoxingView.vue
@@ -1,57 +1,128 @@
 <template>
-  <div class="p-4 relative flex flex-col">
-    <!-- HEADER -->
-    <shadow-header-box />
-    <!-- SET LEVEL -->
+  <div class="grid grid-cols-1 gap-2 md:gap-3 p-4 relative overflow-hidden">
+    <!-- MAIN COLUMN -->
+    <div class="grid grid-cols-full lg:grid-cols-2 gap-2 md:gap-8 2xl:pt-28 pt-7">
+      <div
+        class="flex flex-col justify-between text-almost-white font-public-sans text-justify relative"
+      >
+        <!-- HEADING -->
+        <h1
+          class="text-lg sm:text-3xl lg:text-xl xl:text-3xl 2xl:text-4xl indent-10 font-semibold mb-5 animate-text-show-up"
+        >
+          Set up your shadow-boxing session, follow the commands to execute punches or slips
+          accordingly. Remember to maintain active footwork and head movement.
+        </h1>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 font-public-sans mb-2 h-full">
-      <div class="relative overflow-hidden">
+        <!-- DESCRIPTION -->
+        <p
+          class="text-sm sm:text-lg lg:text-sm xl:text-base 2xl:text-xl animate-text-show-up md:mb-5 h-0 md:h-auto"
+        >
+          The numbering system used for different punches in boxing is purposefully designed to aid
+          in remembering and developing winning combinations. It's based on a traditional boxing
+          stance and follows a formula where even-numbered punches are right-hand punches and
+          odd-numbered punches are left-hand punches.
+        </p>
+
+        <!-- SELECT LEVEL SECTION -->
+        <div>
+          <!-- SELECT LEVEL HEADER -->
+          <h2
+            class="text-xl 2xl:text-3xl pb-3 font-semibold flex items-center border-t py-4 animate-appear-slow-and-delayed"
+          >
+            Select level
+          </h2>
+
+          <!-- LEVEL BUTTONS & DESCRIPTION SECTION -->
+          <div class="grid md:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-2">
+            <!--BASIC LEVEL BUTTON -->
+            <level-selector
+              level="Basic"
+              @click="(basicSelected = true) && (advancedSelected = false)"
+              class="hover:bg-red-500 active:bg-almost-white active:text-almost-black duration-200"
+              :class="{ 'bg-red-500': basicSelected, 'bg-none': advancedSelected }"
+            >
+              <ul class="list-disc md:block hidden">
+                <!-- BASIC LEVEL DESCRIPTION / SLOT USED -->
+                <li class="pb-2">
+                  Suitable for beginners and those who are looking for a less intense workout;
+                </li>
+                <li class="pb-2">
+                  Focuses on basic combinations and slower pace to help users grasp fundamental
+                  movements;
+                </li>
+                <li class="pb-2">
+                  Audio commands and simplified figure which demonstrates each punch, making it
+                  easier to follow along;
+                </li>
+              </ul>
+            </level-selector>
+
+            <!-- ADVANCED LEVEL BUTTON -->
+            <level-selector
+              level="Advanced"
+              @click="(advancedSelected = true) && (basicSelected = false)"
+              class="hover:bg-jaffa active:bg-almost-white active:text-almost-black duration-200"
+              :class="{ 'bg-jaffa': advancedSelected, 'bg-none': basicSelected }"
+            >
+              <!-- ADVANCED LEVEL DESCRIPTION / SLOT USED -->
+              <ul class="list-disc md:block hidden">
+                <li class="pb-2">
+                  Designed for advanced users aiming for a high-intensity workout.
+                </li>
+                <li class="pb-2">Emphasizes rapid movements and more complex combinations;</li>
+                <li class="pb-2">
+                  Audio commands allows helps users to stay focused on the movements.
+                </li>
+              </ul>
+            </level-selector>
+          </div>
+        </div>
+      </div>
+
+      <!-- 2ND COLUMN - HERO IMAGE & CONFIGURATOR SECTION -->
+      <div class="h-full flex flex-col relative min-h-[33rem]">
+        <!-- HERO IMAGE -->
+        <div class="animate-appear-slow-and-delayed">
+          <img
+            src="@/assets/photos/hero-halftone-2_opt.webp"
+            alt="halftone-image-woman-during-boxing-training"
+            class="absolute inset-0 h-full w-full object-none opacity-35 -z-10"
+          />
+        </div>
+
+        <!-- BASIC / ADVANCED COMPONENT WITH VUE TRANSITIONS -->
         <Transition
           mode="out-in"
           enter-active-class="transition-transform duration-1000"
           leave-active-class="transition-transform duration-1000"
-          enter-from-class="transform translate-y-full"
-          leave-to-class="transform translate-y-full"
+          enter-from-class="transform translate-x-full"
+          leave-to-class="transform translate-x-full"
         >
-          <level-configurator
-            v-if="basicSelected"
-            class="absolute inset-0 top-0 left-0"
-            @click="basicSelected = false"
+          <component
+            v-if="basicSelected || advancedSelected"
+            :is="switchingConfigurator"
+            class="absolute inset-0"
           />
         </Transition>
-        <level-description-box level="Basic" @click="basicSelected = true">
-          <ul class="list-disc">
-            <li>Suitable for beginners and those who are looking for a less intense workout;</li>
-            <li>
-              Focuses on basic combinations and slower pace to help users grasp fundamental
-              movements;
-            </li>
-            <li>
-              Audio commands and simplified figure which demonstrates each punch, making it easier
-              to follow along;
-            </li>
-          </ul>
-        </level-description-box>
       </div>
-      <level-description-box level="Advanced">
-        <ul class="list-disc">
-          <li>Designed for advanced users aiming for a high-intensity workout.</li>
-          <li>Emphasizes rapid movements and more complex combinations;</li>
-          <li>Audio commands allows helps users to stay focused on the movements.</li>
-        </ul>
-      </level-description-box>
     </div>
-    <!-- SHADOW BOXING GENERAL DESCRIPTION -->
-    <general-description class="mt-auto" />
   </div>
 </template>
 <script setup lang="ts">
 // component import
-import LevelDescriptionBox from '@/components/shadow_boxing/helpers/LevelDescriptionBox.vue'
-import ShadowHeaderBox from '@/components/shadow_boxing/helpers/ShadowHeaderBox.vue'
-import GeneralDescription from '@/components/shadow_boxing/helpers/GeneralDescription.vue'
-import LevelConfigurator from '@/components/shadow_boxing/helpers/LevelConfigurator.vue'
+import LevelSelector from '@/components/shadow_boxing/LevelSelector.vue'
+import BasicLevelConfigurator from '@/components/shadow_boxing/BasicLevelConfigurator.vue'
+import AdvancedLeveleConfigurator from '@/components/shadow_boxing/AdvancedLevelConfigurator.vue'
 
-import { ref } from 'vue'
+// vue import
+import { ref, computed } from 'vue'
+
+// clicked button indicators - helps to handle displayed configurator and dynamic classes (change color on @click)
 const basicSelected = ref<boolean>(false)
+const advancedSelected = ref<boolean>(false)
+
+// displayed configurator handler
+const switchingConfigurator = computed(() => {
+  return basicSelected.value === true ? BasicLevelConfigurator : AdvancedLeveleConfigurator
+})
 </script>
