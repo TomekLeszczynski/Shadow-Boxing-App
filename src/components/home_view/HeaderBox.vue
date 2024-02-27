2a25a054d3cd80565d8ecc692e1945bfff067ff6
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-02-27 20:04:45 +0100

    components removed. home view code consolidated

--- src/components/home_view/HeaderBox.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <h1
-    class="text-red-500 font-semibold font-public-sans text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-[9rem] tracking-tighter py-3 lg:mb-12"
-  >
-    Shadow Boxing Workout
-  </h1>
-</template>
