d55a8a28425d21943e58470557d4edde88c80ea2
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-02-27 20:31:48 +0100

    comments removed, code consolidation

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -2,7 +2,12 @@
   <div class="max-w-100-lg mx-auto max-h-100 flex-grow flex justify-center items-center">
     <div class="bg-boxingGrey p-5 md:p-8">
       <!-- HEADER -->
-      <sign-up-header />
+      <h1
+        aria-label="create-account-heading"
+        class="text-black md:text-9xl text-6xl font-bold py-4 uppercase"
+      >
+        Create Account
+      </h1>
       <!-- SIGN UP FORM -->
       <sign-up-form />
     </div>
@@ -10,17 +15,11 @@
 </template>
 
 <script setup lang="ts">
-// vue import
 import { onBeforeUnmount } from 'vue'
 
-// import component
 import SignUpForm from '@/components/sign_up/SignUpForm.vue'
-import SignUpHeader from '@/components/sign_up/SignUpHeader.vue'
 
-// pinia import
 import { useAuthStore } from '@/stores/AuthentificationStore'
-
-// store instance
 const authStore = useAuthStore()
 
 onBeforeUnmount(() => {
