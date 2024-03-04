32c13366f46a2b81728910a1ca519e3e8f0d738e
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-03-04 19:44:45 +0100

    comments removed, color names updated

--- src/components/sign_up/SignUpForm.vue
+++ src/components/sign_up/SignUpForm.vue
@@ -9,13 +9,13 @@
         id="displayName"
         v-model="userData.displayName"
         type="text"
-        class="h-10 my-1 px-4 w-full bg-white border-none"
+        class="h-10 my-1 px-4 w-full bg-almost-white border-none"
         placeholder="Rocky Balboa"
         :aria-invalid="v$.displayName.$error"
       />
       <!-- VALIDATION ERROR DISPLAY -->
       <div class="h-5" aria-live="assertive">
-        <span v-if="v$.displayName.$error" class=" text-boxingRed"
+        <span v-if="v$.displayName.$error" class="text-red-500"
           >{{ v$.displayName.$errors[0]?.$message }}
         </span>
       </div>
@@ -29,13 +29,13 @@
         id="email"
         v-model="userData.email"
         type="email"
-        class="h-10 my-1 px-4 w-full bg-white border-none"
+        class="h-10 my-1 px-4 w-full bg-almost-white border-none"
         placeholder="example@mail.com"
         :aria-invalid="v$.email.$error"
       />
       <!-- VALIDATION ERROR DISPLAY -->
       <div class="h-5" aria-live="assertive">
-        <span v-if="v$.email.$error" class=" text-boxingRed">{{
+        <span v-if="v$.email.$error" class="text-red-500">{{
           v$.email.$errors[0].$message
         }}</span>
       </div>
@@ -43,19 +43,19 @@
 
     <!-- PASSWORD INPUT  -->
     <div class="md:col-span-5">
-      <label for="password" class="text-lg text-black">Password</label>
+      <label for="password" class="text-lg text-almost-black">Password</label>
       <input
         @blur="v$.password.password.$touch"
         id="password"
         v-model="userData.password.password"
         type="password"
-        class="h-10 my-1 px-4 w-full bg-white border-none"
+        class="h-10 my-1 px-4 w-full bg-almost-white border-none"
         placeholder="********"
         :aria-invalid="v$.password.password.$error"
       />
       <!-- VALIDATION ERROR DISPLAY -->
       <div class="h-5" aria-live="assertive">
-        <span v-if="v$.password.password.$error" class=" text-boxingRed"
+        <span v-if="v$.password.password.$error" class="text-red-500"
           >{{ v$.password.password.$errors[0].$message }}
         </span>
       </div>
@@ -69,23 +69,23 @@
         id="confirmPassword"
         v-model="userData.password.confirm"
         type="password"
-        class="h-10 my-1 px-4 w-full bg-white border-none"
+        class="h-10 my-1 px-4 w-full bg-almost-white border-none"
         placeholder="********"
         :aria-invalid="v$.password.confirm.$error"
       />
       <!-- VALIDATION ERROR DISPLAY -->
       <div class="h-5" aria-live="assertive">
-        <span v-if="v$.password.confirm.$error" class=" text-boxingRed">{{
+        <span v-if="v$.password.confirm.$error" class="text-red-500">{{
           v$.password.confirm.$errors[0].$message
         }}</span>
       </div>
     </div>
     <!-- ERROR DISPLAY -->
-    <p class="h-5 mt-5 font-semibold text-base text-boxingRed">
-      {{ authStore.authError }} Tutaj pojawi się błąd jeśli nie uda się zarejestrować użytkownika!
+    <p class="h-5 mt-5 font-semibold text-base text-red-500" aria-label="error message">
+      {{ authStore.authError }}
     </p>
 
-    <div class="sm:flex justify-between items-center pt-6 text-black">
+    <div class="sm:flex justify-between items-center pt-6 text-almost-black">
       <!-- ALREADY-A-MEMBER LINK -->
       <div class="text-lg">
         <span>Already a Member?</span>
@@ -99,13 +99,13 @@
         @click="registerUser"
         :disabled="v$.$invalid"
         :class="!v$.$invalid ? 'opacity-100' : 'opacity-50'"
-        class="bg-black text-white text-lg font-semibold mt-3 sm:mt-0 py-4 px-12 group tracking-wide w-full sm:w-auto"
+        class="bg-almost-black text-almost-white text-lg font-semibold mt-3 sm:mt-0 py-4 px-12 group tracking-wide w-full sm:w-auto"
         :aria-disabled="v$.$invalid ? 'true' : 'false'"
       >
         <!-- IS LOADING BUTTON LABEL -->
         <proceding-label v-if="inProgress" procedingLabel="Sending" />
         <!-- DEFAULT BUTTON LABEL -->
-        <div v-if="!inProgress" class="flex flex-row items-center justify-center">
+        <div v-else class="flex flex-row items-center justify-center">
           <span>Sign up</span>
           <span
             :class="{
@@ -130,29 +130,23 @@
 </template>
 
 <script setup lang="ts">
-// vue import
 import { computed, reactive, ref } from 'vue'
-
-// router import
 import { useRouter, RouterLink } from 'vue-router'
-
-// pinia import
 import { useAuthStore } from '@/stores/AuthentificationStore'
 
 // vuelidate import
 import useValidate from '@vuelidate/core'
 import { required, email, minLength, alphaNum, sameAs } from '@vuelidate/validators'
 
-// interface import
-import { type ValidationUserData } from '@/interfaces'
-
-// router instance
-const router = useRouter()
-
-// store instance
-const authStore = useAuthStore()
-
 // user data (due to Vuelidate docs)
+interface ValidationUserData {
+  displayName: string
+  email: string
+  password: {
+    password: string
+    confirm: string
+  }
+}
 const userData = reactive<ValidationUserData>({
   displayName: '',
   email: '',
@@ -180,11 +174,13 @@ const validationRules = computed(() => {
 // vuelidate instance
 const v$ = useValidate(validationRules, userData)
 
-// loading spinner contoller
+// registering new user
+
+// 'Submit' button enabled only when Vuelidate validation is possitive
+const router = useRouter()
+const authStore = useAuthStore()
 const inProgress = ref<boolean>(false)
 
-// registering new user
-// 'Submit' button enabled only when Vuelidate validation is possitive
 const registerUser = (): void => {
   // run inProgress button setup
   inProgress.value = true
