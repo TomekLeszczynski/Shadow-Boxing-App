84919b20e40a9b6d8f6382833e04a56ebe064de5
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-03-13 19:29:05 +0100

    slot added

--- src/components/shadow_boxing/helpers/PlusMinusButton.vue
+++ src/components/shadow_boxing/helpers/PlusMinusButton.vue
@@ -2,5 +2,6 @@
   <button
     class="md:w-1/4 flex-1 text-2xl text-custom-black py-1 hover:bg-custom-grey hover:bg-opacity-20 active:text-custom-white active:bg-custom-orange duration-100 flex justify-center items-center"
   >
+    <slot />
   </button>
 </template>
