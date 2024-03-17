da30b23f2a202685a50b188de52fd8f20eec10ca
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-03-17 16:26:02 +0100

    helper-components of level configurators deleted

--- src/components/shadow_boxing/helpers/BasicConfigLabel.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <label
-    class="text-lg block cursor-pointer select-none py-2 text-center bg-custom-white bg-opacity-20 peer-checked:bg-red-500 peer-checked:font-bold peer-checked:text-custom-white hover:bg-custom-grey hover:bg-opacity-20"
-    >{{ buttonLabel }}</label
-  >
-</template>
-<script setup lang="ts">
-defineProps<{ buttonLabel: string }>()
-</script>
