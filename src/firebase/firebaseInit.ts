408546ead25066f82c2b25f85fee12082c0a2264
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-04-21 13:05:56 +0200

    firebase storage created for storing audio-punches-combinations

--- src/firebase/firebaseInit.ts
+++ src/firebase/firebaseInit.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 export const firebaseApp = initializeApp(firebaseConfig)
 export const db = getFirestore(firebaseApp)
+export const storage = getStorage(firebaseApp)
