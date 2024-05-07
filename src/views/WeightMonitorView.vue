d4efe1631027fa4ba2939703e3e24976cbe46db0
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-05-07 21:19:14 +0200

    weight monitor view set up

--- src/views/WeightMonitorView.vue
+++ src/views/WeightMonitorView.vue
@@ -1,3 +1,10 @@
 <template>
-  <div><h1 class="text-9xl text-white">Weight Monitor View</h1></div>
+  <div class="w-full relative">
+    <weight-input />
+    <stats-display />
+  </div>
 </template>
+<script setup lang="ts">
+import WeightInput from '@/components/weight_monitor/WeightInput.vue'
+import StatsDisplay from '@/components/weight_monitor/StatsDisplay.vue'
+</script>
