f7991b16f30658d0f86dcb14d477f992dcb9623f
Tomek Leszczynski <hiszpania2018@gmail.com>
2024-05-14 19:45:41 +0200

    weight monitor rebuild: measurement input, stats boxes, chart added

--- src/views/WeightMonitorView.vue
+++ src/views/WeightMonitorView.vue
@@ -1,10 +1,142 @@
 <template>
-  <div class="w-full relative">
-    <weight-input />
-    <stats-display />
+  <div class="grid grid-rows-12 p-4 w-full">
+    <!-- ADD NEW VALUE -->
+    <div class="row-start-2 row-end-3 grid justify-center">
+      <weight-input />
+    </div>
+    <!-- DETAILS BOXES -->
+    <div v-if="measurements.length > 0" class="grid row-start-3 row-end-7">
+      <div class="grid grid-cols-5 h-full divide-x-2 border-custom-black pt-9">
+        <stats-box
+          title="Current weight:"
+          :value="computedDetails.lastMeasure"
+          class="animate-text-show-up"
+        />
+        <stats-box
+          title="Progress / regress:"
+          :value="computedDetails.progress"
+          class="animate-text-show-up-02"
+          :valueColor="computedDetails.progress > 0 ? 'text-red-500' : 'text-green-500'"
+        />
+        <stats-box
+          title="Lowest weight:"
+          :value="computedDetails.minValue"
+          class="animate-text-show-up-03"
+        />
+        <stats-box
+          title="Highest weight:"
+          :value="computedDetails.maxValue"
+          class="animate-text-show-up-02"
+        />
+        <stats-box
+          title="Average weight:"
+          :value="computedDetails.avgValue"
+          class="animate-text-show-up"
+        />
+      </div>
+    </div>
+    <!-- CHART -->
+    <div class="grid row-start-7 row-end-13 w-full relative border-t border-custom-black">
+      <weight-display v-if="measurements.length > 0" :measurements="measurements" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+// vue import
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue'
+
+// component import
 import WeightInput from '@/components/weight_monitor/WeightInput.vue'
-import StatsDisplay from '@/components/weight_monitor/StatsDisplay.vue'
+import StatsBox from '@/components/weight_monitor/helpers/StatsBox.vue'
+import WeightDisplay from '@/components/weight_monitor/WeightDisplay.vue'
+
+// helpers import
+import { getDetails } from '@/components/weight_monitor/helpers/handleMeasurements'
+import { type WeightData } from '@/components/weight_monitor/helpers/interfaces'
+
+// firebase imports
+import {
+  collection,
+  query,
+  orderBy,
+  getDocs,
+  onSnapshot,
+  type DocumentData,
+  Query
+} from 'firebase/firestore'
+import type { Unsubscribe } from 'firebase/auth'
+import { db } from '@/firebase/firebaseInit'
+
+// pinia import
+import { useAuthStore } from '@/stores/AuthentificationStore'
+
+// pinia instance
+const authStore = useAuthStore()
+
+// array of user's measurements - includes all measures
+
+const measurements = ref<WeightData[]>([])
+
+//...
+let q: Query<DocumentData, DocumentData>
+
+// get realtime updates
+let unsubscribe: Unsubscribe
+
+// map reveived data to fit WeightData interface
+const mapSnapshot = (snapshot: DocumentData[]): WeightData[] => {
+  return snapshot.map((doc) => ({
+    weight: doc.data().weight,
+    date: doc.data().date.toDate()
+  }))
+}
+
+// modyfing measurements by imported math functions - get by details box
+const computedDetails = computed(() => getDetails(measurements.value))
+
+// check if user is logged in and get data stored in firebase - according to firbase docs.
+const getMeasures = async (): Promise<void> => {
+  if (authStore.user != null) {
+    const measurementsCollection = collection(db, 'users', authStore.user.uid, 'measurements')
+    q = query(measurementsCollection, orderBy('date'))
+    try {
+      const querySnapshot = await getDocs(q)
+      measurements.value = mapSnapshot(querySnapshot.docs)
+    } catch (error) {
+      console.error('getMeasures error: ' + error)
+    }
+  }
+}
+
+// get user id before sending request to firebase / otherwise user = null
+const getUser = async (): Promise<void> => {
+  try {
+    await authStore.checkUserAuthState()
+  } catch (error) {
+    console.error('Get user error: ' + error)
+  }
+}
+
+// get user data and then get his measurements stored in firebase
+onMounted(async () => {
+  try {
+    await getUser()
+    getMeasures()
+    // unsubscribe: according to firebase docs; updates data in real time
+    if (q) {
+      unsubscribe = onSnapshot(q, (snapshot: { docs: DocumentData[] }) => {
+        measurements.value = mapSnapshot(snapshot.docs)
+      })
+    }
+  } catch (error) {
+    console.error('onMounted error:' + error)
+  }
+})
+
+// detach a listener when unmounting component - method follows firebase docs
+onBeforeUnmount(() => {
+  if (unsubscribe) {
+    unsubscribe()
+  }
+})
 </script>
